28463a1065d3733787b2188514c35e8a570cb78d
Krzysztof Kamieniecki <krzysztof.kamieniecki@crealogix.com>
2021-01-19 17:53:20 +0100

    Observable from of and from

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {of, from} from 'rxjs'
 
 @Component({
-  selector: 'pm-root',
+  selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  pageTitle = 'Acme Product Management';
+export class AppComponent implements OnInit {
+  name = 'Angular';
+
+  ngOnInit() {
+    of(2,4,6,8).subscribe(console.log)
+
+    from([20,30,136]).subscribe(
+      item => console.log(`resulting item... ${item}`),
+      err => console.log(`error occured ${err}`),
+      ()=> console.log('complete')
+    )
+    of(['Apple1', 'Apple2', 'Apple3']).subscribe(
+      apple => console.log(`Apple was emitted ${apple}`),
+      err => console.log(`error occured ${err}`),
+      ()=> console.log('No more apples: completed')
+    )
+
+  }
 }
