c062f10cc015e39e59cd6ef281939b64d3700511
Krzysztof Kamieniecki <krzysztof.kamieniecki@crealogix.com>
2021-01-20 12:48:39 +0100

    Operators map, tap, take

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {of, from} from 'rxjs'
+import {map, tap, take} from 'rxjs/operators'
 
 @Component({
   selector: 'my-app',
@@ -12,7 +13,18 @@ export class AppComponent implements OnInit {
   ngOnInit() {
     of(2,4,6,8).subscribe(console.log)
 
-    from([20,30,136]).subscribe(
+    from([20,30,136,5]).pipe(
+      tap(item => console.log(`emitted item ... ${item}`)),
+      map(item => item * 2),
+      map(item => item - 10),
+      map(item => {
+        if (item === 0) {
+          throw new Error('zero detected')
+        }
+        return item
+      }), 
+      take(3) // 5 will be not taken
+    ).subscribe(
       item => console.log(`resulting item... ${item}`),
       err => console.log(`error occured ${err}`),
       ()=> console.log('complete')
